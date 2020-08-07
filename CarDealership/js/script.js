2ff95025018dd612171117b4dc35307f880a19f5
Balwant <bbirajdar7@gmail.com>
2020-08-07 21:15:21 +0530

    completed car Dealership Project

--- CarDealership/js/script.js
+++ CarDealership/js/script.js
@@ -80,3 +80,80 @@ const DisplaySpecialCars = (() => {
         })
     })
 })(createCars);
+
+const DisplayCars = ((createCars) => {
+    const cars = createCars.cars;
+    // console.log(cars);
+
+    const inventory = document.querySelector('.inventory-container');
+
+    document.addEventListener('DOMContentLoaded', () => {
+        inventory.innerHTML = '';
+
+        let output = '';
+        cars.forEach((car) => {
+            output += ` <!-- single car -->
+            <div class="col-10 mx-auto my-3 col-md-6 col-lg-4 single-car ${car.country}">
+              <div class="card car-card">
+                <img
+                  src="${car.img}"
+                  class="card-img-top car-img"
+                  alt=""
+                />
+                <!-- card body -->
+                <div class="card-body">
+                  <div class="car-info d-flex justify-content-between">
+                    <!-- first flex child -->
+                    <div class="car-text text-uppercase">
+                      <h6 class="font-weight-bold">${car.make}</h6>
+                      <h6>${car.model}</h6>
+                    </div>
+                    <!-- second flex child -->
+                    <h5 class="car-value align-self-center py-2 px-3">
+                      $
+                      <span class="car-price">${car.price}</span>
+                    </h5>
+                  </div>
+                </div>
+                <!-- end of card -->
+                <div
+                  class="card-footer text-capitalize d-flex justify-content-between"
+                >
+                  <p>
+                    <span><i class="fas fa-car"></i></span>${car.type}
+                  </p>
+                  <p>
+                    <span><i class="fas fa-cogs"></i></span>${car.trans}
+                  </p>
+                  <p>
+                    <span><i class="fas fa-gas-pump"></i></span>${car.gas}
+                  </p>
+                </div>
+              </div>
+            </div>
+            <!--end of single car -->`
+        })
+        inventory.innerHTML = output;
+    })
+})(createCars);
+
+
+//filterCars
+const filterCars = (() => {
+    const filter = document.querySelectorAll('.filter-btn');
+    console.log(filter);
+    filter.forEach((btn) => {
+        btn.addEventListener('click', (event) => {
+            const value = event.target.dataset.filter;
+            const singleCar = document.querySelectorAll('.single-car');
+            singleCar.forEach(car => {
+                if (value === 'all') {
+                    car.style.display = 'block';
+                } else {
+                    (!car.classList.contains(value)) ? car.style.display = 'none' : car.style.display = 'block';
+                }
+
+            })
+        })
+    })
+})();
